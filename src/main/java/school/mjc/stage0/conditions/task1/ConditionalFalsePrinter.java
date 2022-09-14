0f38b722c00d66d667537bc7bbebd0eb8d4d4953
Olga Pushkina <emmanuilaklein@gmail.com>
2022-09-14 20:35:51 +0300

    all is done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (1 < 0) {
+            System.out.println("false");
+        } else {
+            System.out.println("false");
+        }
     }
 }
+
